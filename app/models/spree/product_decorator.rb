f4b1bc866207bb0bdf472e6e56e3b6f703d47d2e
Knotto <one2treego@hotmail.com>
2019-01-13 11:06:14 +0000

    change group by

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -12,7 +12,8 @@ Spree::Product.class_eval do
     results = Spree::Product.active.select("spree_products.*, SUM(spree_line_items.quantity) as total_qty, spree_line_items.variant_id").
         joins(:line_items).joins("INNER JOIN spree_orders ON spree_orders.id = spree_line_items.order_id").
         where("spree_orders.state = 'complete'").
-        group("spree_line_items.variant_id, spree_products.id")
+        group("spree_products.id")
+        #group("spree_line_items.variant_id, spree_products.id")
 
     unless Spree::Config.show_products_without_price
       results = results.where("spree_prices.amount IS NOT NULL").where("spree_prices.currency" => Spree::Config[:currency])
