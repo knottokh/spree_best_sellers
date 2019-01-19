99b7f523d509ced1e9d56fc27eed4884808adfc5
Knotto <one2treego@hotmail.com>
2019-01-19 12:35:07 +0000

    fix groub by if empty 3

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -9,7 +9,7 @@ Spree::Product.class_eval do
     min_order_create_date = config[:min_order_create_date] || 30
 
 
-    results = Spree::Product.active.select("spree_products.*, SUM(spree_line_items.quantity) as total_qty, spree_line_items.variant_id").
+    results = Spree::Product.active.select("spree_products.*,spree_products.id, SUM(spree_line_items.quantity) as total_qty, spree_line_items.variant_id").
         joins(:line_items).joins("INNER JOIN spree_orders ON spree_orders.id = spree_line_items.order_id").
         where("spree_orders.state = 'complete'")
     
