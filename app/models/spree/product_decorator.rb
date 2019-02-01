542fcbf112bb666938a59d3a7c8588030cd01037
Knotto <one2treego@hotmail.com>
2019-02-01 19:12:16 +0000

    change position group

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -8,15 +8,14 @@ Spree::Product.class_eval do
     config = Spree::BestSellersConfiguration.new
     num_max_best_sellers = config[:max_best_sellers] || 2
     min_order_create_date = config[:min_order_create_date] || 30
-
-
-    results = Spree::Product.active.select("spree_products.*,spree_products.id, SUM(spree_line_items.quantity) as total_qty, spree_line_items.variant_id").
-        joins(:line_items).joins("INNER JOIN spree_orders ON spree_orders.id = spree_line_items.order_id").
-        where("spree_orders.state = 'complete'")
-    
-    if results.present? || !results.empty?
-        results = results.group("spree_line_items.variant_id,spree_products.id")
+    begin
+      results = Spree::Product.active.select("spree_products.*,spree_products.id, SUM(spree_line_items.quantity) as total_qty, spree_line_items.variant_id").
+          joins(:line_items).joins("INNER JOIN spree_orders ON spree_orders.id = spree_line_items.order_id").
+          where("spree_orders.state = 'complete'").group("spree_line_items.variant_id,spree_products.id")
     end
+    #if results.present? || !results.empty?
+    #    results = results.group("spree_line_items.variant_id,spree_products.id")
+    #end
         #group("spree_line_items.variant_id, spree_products.id")
 
     unless Spree::Config.show_products_without_price
