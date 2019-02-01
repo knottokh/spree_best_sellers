7e09f4da5f0b745fc21a428502ba9fe0f853d3d3
Knotto <one2treego@hotmail.com>
2019-02-01 20:26:43 +0000

    remove variant_id

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -9,12 +9,13 @@ Spree::Product.class_eval do
     num_max_best_sellers = config[:max_best_sellers] || 2
     min_order_create_date = config[:min_order_create_date] || 30
     begin
-      results = Spree::Product.active.select("spree_products.*,spree_products.id, SUM(spree_line_items.quantity) as total_qty, spree_line_items.variant_id").
+      results = Spree::Product.active.select("spree_products.*,spree_products.id, SUM(spree_line_items.quantity) as total_qty").
           joins(:line_items).joins("INNER JOIN spree_orders ON spree_orders.id = spree_line_items.order_id").
-          where("spree_orders.state = 'complete'").group("spree_products.id,spree_line_items.variant_id")
+          where("spree_orders.state = 'complete'").group("spree_products.id")
     end
-    #if results.present? || !results.empty?
-    #    results = results.group("spree_line_items.variant_id,spree_products.id")
+    
+    #if results.present?
+    #   results = results.group("spree_products.id")
     #end
         #group("spree_line_items.variant_id, spree_products.id")
 
